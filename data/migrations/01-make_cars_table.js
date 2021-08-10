c7356bac7a07b047be4970f774812e68eb8da533
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-10 12:48:39 -0700

    seed and migration created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.table('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).unique().notNullable()   
+    tbl.string('make', 128).notNullable()
+    tbl.string('model', 128).notNullable()
+    tbl.numeric('mileage').unsigned().notNullable() // cannot be negative number
+    tbl.string('title', 128)
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
-// STRETCH
+const cars = [
+    {
+        vin: '12345678',
+        make: 'Nissan',
+        model: "Sentra",
+        mileage: 243432,
+        title: 'salvage',
+        transmission: 'automatic'
+    },
+    {
+        vin: '09989899',
+        make: 'Nissan',
+        model: "Altima",
+        mileage: 33393,
+        title: 'clean',
+        
+    },
+    {
+        vin: '77888777',
+        make: 'Nissan',
+        model: "Frontier",
+        mileage: 22000,
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(()=> {
+//     return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
